36615d071756546f6187d454fc32029fc1908052
93814690 <93814690@qq.com>
2023-02-06 22:19:47 +0800

    update: 更新api模块

--- fly-push-api/src/main/java/top/liyf/fly/push/api/feign/ChanifyClient.java
+++ fly-push-api/src/main/java/top/liyf/fly/push/api/feign/ChanifyClient.java
@@ -1,16 +1,16 @@
-package top.liyf.fly.push.api.api;
+package top.liyf.fly.push.api.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import top.liyf.fly.push.api.domain.ChanifyText;
 
 /**
  * @author liyf
  * Created in 2021-05-26
  */
-@RequestMapping("/chanify")
-public interface ChanifyApi {
+@FeignClient(value = "fly-push", path = "/chanify")
+public interface ChanifyClient {
 
     @PostMapping("/text")
     void text(@RequestBody ChanifyText text);
--- src/main/java/top/liyf/fly/push/api/ChanifyController.java
+++ src/main/java/top/liyf/fly/push/api/ChanifyController.java
@@ -3,9 +3,9 @@ package top.liyf.fly.push.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import top.liyf.fly.push.api.api.ChanifyApi;
 import top.liyf.fly.push.api.domain.ChanifyText;
 import top.liyf.fly.push.config.ChanifyConfig;
 import top.liyf.fly.push.util.ChanifyUtils;
@@ -17,14 +17,15 @@ import java.util.Map;
  * Created in 2021-05-13
  */
 @RestController
-public class ChanifyController implements ChanifyApi {
+@RequestMapping("/chanify")
+public class ChanifyController {
 
     @Autowired
     RestTemplate restTemplate;
     @Autowired
     ChanifyConfig chanifyConfig;
 
-    @Override
+    @PostMapping("/text")
     public void text(@RequestBody ChanifyText text) {
         System.out.println("text = " + text);
         String url = chanifyConfig.getHost() + ChanifyUtils.TEXT;
